3336fab7b1e0af85059894c9c402187e7f9b6d40
HJ Kang <hkang07@gmail.com>
2023-02-26 22:20:23 +0000

    project setup done

---
+++ index.js
@@ -0,0 +1,26 @@
+const app = require('./src/app');
+
+app.get('/', (req, res) => {
+  res.status(200).json({ hello: 'world' });
+});
+
+// error-handling middleware
+app.use((req, res, next) => {
+  const err = new Error('Not found');
+  err.status = 404;
+
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    success: false,
+    message: err.message || 'Something went wrong'
+  });
+});
+
+const APP_PORT = 4000;
+
+app.listen(APP_PORT, () => {
+  console.log(`App is listening on port ${APP_PORT}`);
+});
